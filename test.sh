e2b46bf91bd2d9c071bd4beb75002acffcfd8b1c
Fabio Henrique Gonzales Barbosa <fabiohbarbosa@gmail.com>
2017-11-16 16:12:22 -0200

    Trying fixing CI error

--- test.sh
+++ test.sh
@@ -14,7 +14,7 @@ else
     echo 'Do not exclude container because there is no'
 fi
 
-docker run -v ${volume} -i -t -e lock=false --name ${container} ${tag}
+docker run -v ${volume} -it -e lock=false --name ${container} ${tag}
 docker_exit_code=`expr $?`
 
 if [[ ${docker_exit_code} != 0 ]]; then
