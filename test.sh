9df93d9818716a7ae0e659eb6af6ee1cc5635b8c
Fabio Henrique Gonzales Barbosa <fabiohbarbosa@gmail.com>
2017-11-17 09:01:51 -0200

    Testing cache

--- test.sh
+++ test.sh
@@ -14,7 +14,7 @@ else
     echo 'Do not exclude container because there is no'
 fi
 
-docker run -v ${volume} -e lock=false --name ${container} ${tag}
+docker run -v ${volume} -e lock=true --name ${container} ${tag}
 docker_exit_code=`expr $?`
 
 if [[ ${docker_exit_code} != 0 ]]; then
